84fe566c38ff505ba896380bdf3921aac321ae7e
Hussnainulhaq <89554931+Hussnainulhaq@users.noreply.github.com>
2024-06-25 21:39:18 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import CountrySearch from './components/CountrySearch';
+import './App.css';
+
+function App() {
+  return (
+      <div className="App">
+        <div className="container">
+          <CountrySearch />
+        </div>
+      </div>
+  );
+}
+
+export default App;
